8a8bce34849d847e526501419445835445ac1f8f
Roragok <Roragok@gmail.com>
2015-12-17 15:43:31 -0500

    move files and shit arond

---
+++ sites/all/themes/roragok_theme/templates/includes/footer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PhiL
+ * Date: 12/17/2015
+ * Time: 2:54 PM
+ */
+?>
+<div class="footer-wrapper">
+    <?php print render($page['bottom']); ?>
+</div>
+
